a3a1a34cea0908c6f586432b7b9c8ef4f79fc2a6
salty-max <max@jellycat.fr>
2024-04-06 21:33:37 +0200

    fix: build

--- packages/ui/src/main.tsx
+++ packages/ui/src/main.tsx
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider, TooltipProvider } from '../lib/components/index.tsx';
 import '../lib/styles/style.css';
 import App from './App.tsx';
-import { LocaleProvider } from '../lib/components/locale/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
-      <LocaleProvider locale='en'>
-        <TooltipProvider>
-          <App />
-        </TooltipProvider>
-      </LocaleProvider>
+      <TooltipProvider>
+        <App />
+      </TooltipProvider>
     </ThemeProvider>
   </React.StrictMode>,
 );
